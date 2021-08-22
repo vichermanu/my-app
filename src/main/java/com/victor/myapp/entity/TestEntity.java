11f78c8b2b06db8dd4dd9b8ac61a129c3023dba2
vichermanu <vichermt@gmail.com>
2021-08-22 14:07:51 -0500

    nombre:my-app descripcio:vercion final del proyecto

---
+++ src/main/java/com/victor/myapp/entity/TestEntity.java
@@ -0,0 +1,27 @@
+package com.victor.myapp.entity;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestEntity {
+	/*
+	 * propiedad prueva para compilacion de archivo testEntity
+	 */
+    private String nombre;
+/*
+ * Return nombre
+ */
+	public String getNombre() {
+		return nombre;
+	}
+/*
+ * parametro nombre the nombre to set
+ */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+    
+	
+	
+}
